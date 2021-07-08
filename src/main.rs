7bf1ca9c3e186a0a7507639919bed4c17fdd0857
Christopher Chang <chiggiechang@gmail.com>
2021-07-07 18:34:21 -0700

    Move natural sort comparison to another file, begin displaying prev dir

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod natural_sort; // This declares the exiswtence of the natural_sort module, which searches by
+                  // default for natural_sort.rs or natural_sort/mod.rs
+
 use std::cmp::Ordering;
 use std::fs::DirEntry;
 use std::io::{self, Write};
 use std::path::Path;
 use std::vec::Vec;
+use natural_sort::cmp_natural;
 
 use crossterm::{
     cursor,
@@ -16,6 +20,30 @@ use crossterm::{
 fn main() -> Result<()> {
     let mut w = io::stdout();
 
+    execute!(w, terminal::EnterAlternateScreen)?;
+
+    terminal::enable_raw_mode()?;
+
+    let result = run(&mut w);
+
+    execute!(
+        w,
+        style::ResetColor,
+        cursor::Show,
+        terminal::LeaveAlternateScreen,
+    )?;
+
+    terminal::disable_raw_mode()?;
+
+    match result {
+        Ok(_) => println!("Goodbye."),
+        Err(err) => panic!(err),
+    }
+
+    Ok(())
+}
+
+fn run(mut w: &mut io::Stdout) -> Result<()> {
     let user_name = match std::env::var("USER") {
         Ok(val) => val,
         Err(e) => panic!("Could not read $USER environment variable: {}", e),
@@ -35,10 +63,6 @@ fn main() -> Result<()> {
 
     let home_path = Path::new(&home_name[..]);
 
-    execute!(w, terminal::EnterAlternateScreen)?;
-
-    terminal::enable_raw_mode()?;
-
     // NOTE(Chris): The default column ratio is 1:2:3
 
     let mut entry_index = 0;
@@ -49,9 +73,16 @@ fn main() -> Result<()> {
 
     let mut entries = get_sorted_entries(&current_dir);
 
+    // TODO(Chris): Eliminate prev_dir variable entirely, since it seems to be unnecessary
+    let mut prev_dir = current_dir.parent().unwrap().to_path_buf();
+
+    let mut prev_entries = get_sorted_entries(&prev_dir);
+
     // Main input loop
     loop {
         // TODO(Chris): Handle case when current_dir is '/'
+        // NOTE(Chris): This creates a new String, and it'd be nice to avoid making a heap
+        // allocation here, but it's probably not worth trying to figure out how to use only a str
         let current_dir_display = if current_dir.starts_with(home_path) {
             // "~"
             format!(
@@ -63,9 +94,6 @@ fn main() -> Result<()> {
                     .unwrap()
             )
         } else {
-            // TODO(Chris): Figure out how to use simply a str type both here and in its
-            // the corresponding if clause, rather than creating a new String (and thus
-            // doing a heap allocation).
             current_dir.to_str().unwrap().to_string()
         };
 
@@ -105,7 +133,8 @@ fn main() -> Result<()> {
         // TODO(Chris): Correctly display previous directory column, especially
         // as it relates to the current path.
 
-        queue_entries_column(&mut w, 1, width / 6 - 2, height, &entries, entry_index)?;
+        // TODO(Chris): Update entries_index
+        queue_entries_column(&mut w, 1, width / 6 - 2, height, &prev_entries, 0)?;
 
         queue_entries_column(
             &mut w,
@@ -126,6 +155,9 @@ fn main() -> Result<()> {
                 std::env::set_current_dir("..")?;
                 current_dir = current_dir.parent().unwrap().to_path_buf();
                 entries = get_sorted_entries(&current_dir);
+
+                prev_dir = current_dir.parent().unwrap().to_path_buf();
+                prev_entries = get_sorted_entries(&prev_dir);
             }
             // TODO(Chris): Implement scrolling down to see more entries in large directories
             'j' => {
@@ -149,14 +181,8 @@ fn main() -> Result<()> {
         w,
         style::ResetColor,
         cursor::Show,
-        terminal::LeaveAlternateScreen,
     )?;
 
-    w.flush()?;
-
-    terminal::disable_raw_mode()?;
-
-    println!("Goodbye.");
 
     Ok(())
 }
@@ -185,95 +211,7 @@ fn cmp_dir_entry(entry1: &DirEntry, entry2: &DirEntry) -> Ordering {
     } else if file_type2.is_dir() && file_type1.is_file() {
         return Ordering::Greater;
     } else {
-        return cmp_alphanum(entry1.file_name().to_str().unwrap(), entry2.file_name().to_str().unwrap());
-    }
-}
-
-fn cmp_alphanum(str1: &str, str2: &str) -> Ordering {
-    cmp_alphanum_2(str1, str2)
-}
-
-// NOTE(Chris): This is adapted from lf's natural less implementation, which can be found in its
-// misc.go file.
-// https://github.com/gokcehan/lf/blob/55b9189713f40b5d2058fad7cf77f82d902485f1/misc.go#L173
-// NOTE(Chris): lf's algorithm uses the lo1, lo2, hi1, and hi2 variables to keep track of the
-// "chunks" in each string, comparing them as necessary. By using these index variables, this
-// algorithm doesn't seem to make any heap allocations. Unfortunately, my implementation of the
-// algorithm does.
-fn natural_less(str1: &str, str2: &str) -> bool {
-    // NOTE(Chris): This is going to involve some more
-    // allocations than may be strictly necessary, but
-    // we're doing this so we can easily index chars1
-    // and chars2.
-    let s1: Vec<char> = str1.chars().collect();
-    let s2: Vec<char> = str2.chars().collect();
-
-    let mut lo1: usize;
-    let mut lo2: usize;
-    let mut hi1 = 0;
-    let mut hi2 = 0;
-
-    loop {
-        // Return true if s1 has run out of characters, but s2 still has characters left.  If s2
-        // has also run out of characters, then s1 and s2 are equal (or so I would think), in which
-        // case return false.
-        if hi1 >= s1.len() {
-            return hi2 != s2.len();
-        }
-
-        // Since the previous if block didn't return, s1 has not run out of characters and yet s2
-        // has. So, s2 is a prefix of s1 and really s1 is greater than s2, so return false.
-        if hi2 >= s2.len() {
-            return false;
-        }
-
-        let is_digit_1 = s1[hi1].is_numeric();
-        let is_digit_2 = s2[hi2].is_numeric();
-
-        // This advances lo1 and hi1 to the next chunk, with hi1 being the exclusive last index of
-        // the chunk.
-        lo1 = hi1;
-        while hi1 < s1.len() && s1[hi1].is_numeric() == is_digit_1 {
-            hi1 += 1;
-        }
-
-        // This advances lo2 and hi2 to the next chunk, with hi2 being the exclusive last index of
-        // the chunk.
-        lo2 = hi2;
-        while hi2 < s2.len() && s2[hi2].is_numeric() == is_digit_2 {
-            hi2 += 1;
-        }
-
-        // If the string forms of the chunks are equal, then keep going. We haven't found out the
-        // ordering of the overall strings yet.
-        if s1[lo1..hi1] == s2[lo2..hi2] {
-            continue
-        }
-
-        // If both chunks are digits, then convert them into actual ints and compare them
-        if is_digit_1 && is_digit_2 {
-            // TODO(Chris): Avoid allocating new Strings here
-            let s1: String = s1[lo1..hi1].into_iter().collect();
-            let s2: String = s2[lo2..hi2].into_iter().collect();
-            if let (Ok(num1), Ok(num2)) = (s1.parse::<usize>(), s2.parse::<usize>()) {
-                return num1 < num2;
-            }
-        }
-
-        // If we've made it this far, then neither the string forms of the chunks are equal nor are
-        // both of the chunks actually numerical. Thus, these chunks are the ones which will
-        // finally determine if the order of the strings, so we only need to compare them.
-        return s1[lo1..hi1] < s2[lo2..hi2];
-    }
-}
-
-fn cmp_alphanum_2(str1: &str, str2: &str) -> Ordering {
-    if natural_less(str1, str2) {
-        return Ordering::Less;
-    } else if str1 == str2 {
-        return Ordering::Equal;
-    } else {
-        return Ordering::Greater;
+        return cmp_natural(entry1.file_name().to_str().unwrap(), entry2.file_name().to_str().unwrap());
     }
 }
 
@@ -364,8 +302,6 @@ fn queue_entries_column(
     Ok(())
 }
 
-// TODO(Chris): Change the string sorting to sort numerical values differently
-// i.e. 1, 10, 2 -> 1, 2, 10
 fn get_sorted_entries<P: AsRef<Path>>(path: P) -> Vec<DirEntry> {
     let mut entries = std::fs::read_dir(path)
         .unwrap()
@@ -385,16 +321,16 @@ mod tests {
 
     #[test]
     fn cmp_alphanum_works() {
-        assert_eq!(cmp_alphanum("10.bak", "1.bak"), Ordering::Greater);
-        assert_eq!(cmp_alphanum("1.bak", "10.bak"), Ordering::Less);
+        assert_eq!(cmp_natural("10.bak", "1.bak"), Ordering::Greater);
+        assert_eq!(cmp_natural("1.bak", "10.bak"), Ordering::Less);
 
-        assert_eq!(cmp_alphanum("2.bak", "10.bak"), Ordering::Less);
+        assert_eq!(cmp_natural("2.bak", "10.bak"), Ordering::Less);
 
-        assert_eq!(cmp_alphanum("1.bak", "Cargo.lock"), Ordering::Less);
+        assert_eq!(cmp_natural("1.bak", "Cargo.lock"), Ordering::Less);
 
-        assert_eq!(cmp_alphanum(".gitignore", "src"), Ordering::Less);
+        assert_eq!(cmp_natural(".gitignore", "src"), Ordering::Less);
 
-        assert_eq!(cmp_alphanum(".gitignore", ".gitignore"), Ordering::Equal);
+        assert_eq!(cmp_natural(".gitignore", ".gitignore"), Ordering::Equal);
     }
 
     #[test]
---
+++ src/natural_sort.rs
@@ -0,0 +1,96 @@
+use std::cmp::Ordering;
+
+fn is_digit(b: u8) -> bool {
+    // 48 = '0'
+    // 57 = '9'
+    48 <= b && b <= 57
+}
+
+// NOTE(Chris): This is adapted from lf's natural less implementation, which can be found in its
+// misc.go file.
+// https://github.com/gokcehan/lf/blob/55b9189713f40b5d2058fad7cf77f82d902485f1/misc.go#L173
+// NOTE(Chris): lf's algorithm uses the lo1, lo2, hi1, and hi2 variables to keep track of the
+// "chunks" in each string, comparing them as necessary. By using these index variables, this
+// algorithm doesn't seem to make any heap allocations.
+// TODO(Chris): Profile this implementation to check if there are any heap allocations.
+pub fn cmp_natural(str1: &str, str2: &str) -> Ordering {
+    // if natural_less(str1, str2) {
+    //     return Ordering::Less;
+    // } else if str1 == str2 {
+    //     return Ordering::Equal;
+    // } else {
+    //     return Ordering::Greater;
+    // }
+
+
+    // NOTE(Chris): This is going to involve some more allocations than may be strictly necessary,
+    // but we're doing this so we can easily index chars1 and chars2.
+    // let s1: Vec<char> = str1.chars().collect();
+    // let s2: Vec<char> = str2.chars().collect();
+    let s1 = str1.as_bytes();
+    let s2 = str2.as_bytes();
+
+    let mut lo1: usize;
+    let mut lo2: usize;
+    let mut hi1 = 0;
+    let mut hi2 = 0;
+
+    loop {
+        // Return Less if s1 has run out of characters, but s2 still has characters left. If s2 has
+        // also run out of characters, then s1 and s2 are equal (or so I would think), in which
+        // case return Equal.
+        if hi1 >= s1.len() {
+            if hi2 >= s2.len() {
+                return Ordering::Equal;
+            } else {
+                return Ordering::Less;
+            }
+        }
+
+        // Since the previous if block didn't return, s1 has not run out of characters and yet s2
+        // has. So, s2 is a prefix of s1 and really s1 is greater than s2, so return Greater.
+        if hi2 >= s2.len() {
+            return Ordering::Greater;
+        }
+
+        let is_digit_1 = is_digit(s1[hi1]);
+        let is_digit_2 = is_digit(s2[hi2]);
+
+        // This advances lo1 and hi1 to the next chunk, with hi1 being the exclusive last index of
+        // the chunk.
+        lo1 = hi1;
+        while hi1 < s1.len() && is_digit(s1[hi1]) == is_digit_1 {
+            hi1 += 1;
+        }
+
+        // This advances lo2 and hi2 to the next chunk, with hi2 being the exclusive last index of
+        // the chunk.
+        lo2 = hi2;
+        while hi2 < s2.len() && is_digit(s2[hi2]) == is_digit_2 {
+            hi2 += 1;
+        }
+
+        // If the string forms of the chunks are equal, then keep going. We haven't found out the
+        // ordering of the overall strings yet.
+        if s1[lo1..hi1] == s2[lo2..hi2] {
+            continue
+        }
+
+        // If both chunks are digits, then convert them into actual ints and compare them
+        if is_digit_1 && is_digit_2 {
+            // let s1: String = s1[lo1..hi1].into_iter().collect();
+            // let s2: String = s2[lo2..hi2].into_iter().collect();
+            // TODO(Chris): Log any errors that come from this utf8 conversion
+            let s1 = std::str::from_utf8(&s1[lo1..hi1]).unwrap();
+            let s2 = std::str::from_utf8(&s2[lo2..hi2]).unwrap();
+            if let (Ok(num1), Ok(num2)) = (s1.parse::<usize>(), s2.parse::<usize>()) {
+                return num1.cmp(&num2);
+            }
+        }
+
+        // If we've made it this far, then neither are the string forms of the chunks equal nor are
+        // both of the chunks actually numerical. Thus, these chunks are the ones which will
+        // finally determine the order of the strings, so we only need to compare them.
+        return s1[lo1..hi1].cmp(&s2[lo2..hi2]);
+    }
+}
